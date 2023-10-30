eaf11462416450b73b1924db2fab222a4b3dad3f
Suraj Deshmukh <suraj.deshmukh@microsoft.com>
2023-10-30 04:44:28 +0000

    install-skopeo: Check the minor version of skopeo
    It has to be greater than 1.13.x.
    
    Signed-off-by: Suraj Deshmukh <suraj.deshmukh@microsoft.com>

--- encrypt-image/install-skopeo.sh
+++ encrypt-image/install-skopeo.sh
@@ -8,9 +8,20 @@ fi
 
 # Check if skopeo is already installed then skip this script
 if command -v skopeo >/dev/null 2>&1; then
-    echo "skopeo is already installed, skipping..."
-    exit 0
+    # # Sample output of skopeo --version
+    # $ skopeo --version
+    # skopeo version 1.13.3 commit: 9e29e4cede9bdaa4a54aa5b0af86efedb823bde4
+    skopeo_minor=$(skopeo --version | awk '{print $3}' | cut -d'.' -f2)
+    if [ "$skopeo_minor" -ge 13 ]; then
+        echo "skopeo is already installed, skipping..."
+        exit 0
+    fi
+    echo "skopeo version > 1.13.x needed! Please uninstall the existing skopeo and re-run this script. Current version: $(skopeo --version)."
+    exit 1
 fi
+
+# Compulsory env vars
+: "${GOBIN}:?"
 
 export SKOPEO_VERSION=v1.13.3
 
